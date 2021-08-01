6d37877f3e22c19378e50f2b60738e61bd8bb726
Nolan <nolanhilbersgarcia@gmail.com>
2021-08-01 16:37:48 +0200

    Added dark mode

--- app/src/main/java/com/retsi/dabijhouder/MainActivity.kt
+++ app/src/main/java/com/retsi/dabijhouder/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
@@ -28,6 +29,12 @@ class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val prefs2 = this.getSharedPreferences(SettingsFragment.PREFS_NAME, MODE_PRIVATE)
+        val mode = prefs2.getInt(SettingsFragment.THEME_KEY, AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+
+        AppCompatDelegate.setDefaultNightMode(mode)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
--- app/src/main/java/com/retsi/dabijhouder/SettingsFragment.kt
+++ app/src/main/java/com/retsi/dabijhouder/SettingsFragment.kt
@@ -3,6 +3,7 @@ package com.retsi.dabijhouder
 import android.content.Context
 import android.content.Context.MODE_PRIVATE
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.fragment.findNavController
 import androidx.preference.ListPreference
 import androidx.preference.Preference
@@ -13,11 +14,11 @@ class SettingsFragment : PreferenceFragmentCompat() {
     companion object {
         const val PREFS_NAME = "MyPrefsFile"
         const val LANGUAGE_KEY = "language"
+        const val THEME_KEY = "theme"
     }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
-
         val prefs = requireActivity().getSharedPreferences(PREFS_NAME, MODE_PRIVATE)
         val taal = prefs.getString(LANGUAGE_KEY, "en-us")
 
@@ -25,7 +26,7 @@ class SettingsFragment : PreferenceFragmentCompat() {
         taalPref!!.setDefaultValue(taal)
 
         taalPref.onPreferenceChangeListener =
-            Preference.OnPreferenceChangeListener { preference, newValue ->
+            Preference.OnPreferenceChangeListener { _, newValue ->
 
                 var lang = "en-us"
                 when (newValue) {
@@ -46,6 +47,26 @@ class SettingsFragment : PreferenceFragmentCompat() {
         vakkenPref!!.onPreferenceClickListener = Preference.OnPreferenceClickListener {
             val action = SettingsFragmentDirections.actionSettingsFragmentToAddSubjectsFragment()
             findNavController().navigate(action)
+            true
+        }
+
+        val darkPref = findPreference<ListPreference>("dark_key")
+
+        when (prefs.getInt(THEME_KEY, AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)) {
+            AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM -> darkPref?.setDefaultValue(resources.getStringArray(R.array.themes)[2])
+            AppCompatDelegate.MODE_NIGHT_YES -> darkPref?.setDefaultValue(resources.getStringArray(R.array.themes)[1])
+            AppCompatDelegate.MODE_NIGHT_NO -> darkPref?.setDefaultValue(resources.getStringArray(R.array.themes)[0])
+        }
+
+        darkPref?.setOnPreferenceChangeListener { _, newValue ->
+            when (newValue){
+                "donker" -> {AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    prefs.edit().putInt(THEME_KEY, AppCompatDelegate.MODE_NIGHT_YES).apply()}
+                "licht" -> {AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    prefs.edit().putInt(THEME_KEY, AppCompatDelegate.MODE_NIGHT_NO).apply()}
+                "systeem" -> {AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                    prefs.edit().putInt(THEME_KEY, AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM).apply()}
+            }
             true
         }
 
